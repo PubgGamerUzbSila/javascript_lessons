944c0bf5c1ec3ea90c26d44ac7de4291d43961b7
Sherbek Mavlonov <mavlonovsherbek43@gmail.com>
2023-06-05 18:20:45 +0500

    FINISH: 1-dars

---
+++ 1-dars/index.js
@@ -0,0 +1,114 @@
+/* console.log(10 + 50)
+
+// var text =
+//   'Lorem lorem dolor sit amet Lorem adipisicing elit. Molestiae sapiente harum, nihil alias debitis nam!'
+
+// let text =
+//   'Lorem lorem dolor sit amet Lorem adipisicing elit. Molestiae sapiente harum, nihil alias debitis nam!'
+
+// Data types | Ma'lumot turlari
+
+// 1. String
+// 2. Boolean
+// 3. Number
+// 4. Undefined
+// 5. Null
+// 6. Object
+// 7. BigInt
+// 8. Symbol
+
+*/
+
+// javascript.info => developer mozilla (mdn) => frontendmasters.com => youtube.com
+
+// let
+
+// Mumkin bo'lmagan => // raqam, symbollar bilan (-), ikkitalik so'z  bo'lsa - bilan ajratib yoza olmaymiz, o'zgaruvchi nomi sifatida oldindan band qilib quyilgan javascript keyvordlaridan foydalana olmaymiz.
+
+// Tafsiya qilinmaydiganlari
+
+// let first_name = 'John'
+
+// let firstName = 'John'
+// let firstName = 'John'
+// let firstName = `John`
+// let lastName = 'Doe'
+
+// var firstName = 'John'
+
+// let firstName = 'Nurbek'
+// console.log(firstName)
+
+// firstName = 'Doe'
+
+// console.log(firstName)
+
+// let isMarried = true
+
+// console.log(isMarried)
+
+// isMarried = false
+
+// console.log(isMarried)
+
+// let n = 10.223
+
+// console.log(n)
+
+// n = 30
+
+// console.log(n)
+
+// let firstName = undefined
+
+// console.log(firstName)
+
+// ``
+// let a = 10
+// let b = 20
+
+// console.log("a bilan b ning qo'shilgandagi qiymati: ", a + b)
+
+// console.log(a + ' bilan 10 + 20 ' + b + " ning qo'shilgandagi qiymati: ", a + b)
+
+// Template literals
+// console.log(`${a} bilan ${b} ning qoshilgandagi qiymati: ${a + b}`)
+
+// let y = 2
+// let x = 64
+
+// console.log(x + y)
+// console.log(y - x)
+
+// console.log(x * y)
+// console.log(x / y)
+
+// console.log(y % x)
+
+// console.log(y ** x)
+
+// let a = 80
+// let b = 100
+
+// a = a + b // => 180
+// b = a - b // => 180 - 100
+// a = a - b // => 180 - 80
+
+// console.log(a)
+// console.log(b)
+
+// const n = 876
+
+// console.log((n - (n % 100)) / 100 + ((n % 100) - (n % 10)) / 10 + (n % 10))
+
+// let a = n % 10,
+//   b = ((n % 100) - (n % 10)) / 10,
+//   c = (n - (n % 100)) / 100
+
+// console.log(c + a + b)
+// 3 + 4 + 5 => 12
+
+// let a = 10,
+//   b = '20'
+
+// console.log(a + b)
