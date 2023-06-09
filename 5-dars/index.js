d95a391fcbea7e33852ba5b261b9759b102ff54f
Sherbek Mavlonov <mavlonovsherbek43@gmail.com>
2023-06-09 17:54:18 +0500

    FINISH: 5-dars (loop, nullish, string)

---
+++ 5-dars/index.js
@@ -0,0 +1,175 @@
+'use strict'
+
+// let num = 10
+
+// console.log(num)
+
+// 5 falsy
+
+// undefined, null, 0, "", NaN
+
+// || (or)
+
+// console.log(NaN || 'Salom')
+// console.log(undefined || 'Salom')
+// console.log(null || 'Salom')
+// console.log('' || 'Salom')
+// console.log(0 || 'Salom' || 'Xayr')
+
+// ?? (Nullish) => null, undefined
+
+// console.log(NaN ?? 'Salom')
+// console.log(undefined ?? 'Salom')
+// console.log(null ?? 'Salom')
+// console.log('' ?? 'Salom')
+// console.log(0 ?? 'Salom' ?? 'Xayr')
+// console.log(false ?? 'Salom')
+
+// const a = 1
+
+// if (a == '10') {
+//   console.log('hello world')
+// } else {
+//   console.log('salom dunyo')
+// }
+
+// switch (a) {
+//   case '10':
+//     console.log('hello world')
+//     break
+//   case 10:
+//     console.log('hello world1')
+//     break
+//   default:
+//     console.log('salom dunyo')
+//     break
+// }
+
+// switch (a) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     console.log('Qish')
+//     break
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log('Bahor')
+//     break
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log('Yoz')
+//     break
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log('Kuz')
+//     break
+
+//   default:
+//     break
+// }
+//            01234
+
+// let newText = ''
+// console.log(`${text} so'zidagi belgilar soni ${text.length}`)
+
+// for (let i = 0; i < text.length; i++) {
+//   console.log(text[i])
+// }
+
+// for (let i = text.length - 1; i >= 0; i--) {
+//   newText += text[i]
+// }
+// console.log(newText)
+
+// let i = text.length - 1
+
+// while (i >= 0) {
+//   console.log(text[i])
+//   // i = i + 1
+//   // i += 1
+//   i--
+// }
+
+// const text = 'Salom'
+
+// for (let i = 0; i < text.length; i++) {
+//   // console.log(text[i])
+//   // if (text[i] === 'l') {
+//   //   continue
+//   // }
+// }
+
+// let str = `men o'zbekistonni sevaman jonajon vatanim mangu bo'l omon
+//    Men amerikada turib o'zbekistonni sog'inishni xoxlayman`
+// const str2 = 'amerikada'
+
+// const str = '312321 321321 5443543 54353432 312321'
+
+// men o'zbekistonni sevaman jonajon vatanim mangu bo'l Omon
+// Men O'zbekistonni Sevaman
+
+// Men o'zbekistonni Sevaman
+
+// str[0] == 'M'
+// let newStr = ''
+
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] !== '4') {
+//     newStr += str[i]
+//   }
+// }
+
+// console.log(newStr)
+
+// const str = '12312 44 312312444 3123124 4 444444444'
+// let result = ''
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] === '4' && str[i + 1] !== '4' && str[i - 1] !== '4') {
+//     continue
+//   }
+//   result += str[i]
+// }
+
+// console.log(result)
+
+// console.log(4 !== 4)
+
+// integer
+// integer
+
+// const userKiritganSoz = 'integr'
+
+// const integr = 'integer'
+
+// console.log(integr)
+
+// const n = 4
+
+// console.log(
+//   n == 1
+//     ? 'Qish'
+//     : n === 2
+//     ? 'Bahor'
+//     : n === 3
+//     ? 'Yoz'
+//     : n === 4
+//     ? 'Kuz'
+//     : 'Bunaqa fasl mavjud emas'
+// )
+// const a = 10
+// if (a) {
+//   console.log('Hello world')
+// } else if (a) {
+//   console.log('dunyo')
+// }
+
+// if (a) {
+//   console.log('Salom')
+// }
+
+// if (a) {
+//   console.log('World')
+// }
