e192e660a278b43847b4cd9fd150bf28a1a41c86
Sherbek Mavlonov <mavlonovsherbek43@gmail.com>
2023-06-16 10:07:27 +0500

    FINISH: 9-dars (object)

---
+++ 9-dars/index.js
@@ -0,0 +1,53 @@
+// object
+
+// const color = 'yellow'
+
+// const span = {
+//   border: '1px solid red',
+//   color,
+//   backgroundColor: 'aqua',
+//   'z-index': 1,
+// }
+
+// console.log(span.z - index)
+
+// console.log(span['z-index'])
+
+// console.log(span[ketmon])
+
+// console.log(span.ketmon)
+
+// if (span.color === 'yellow') {
+//   console.log('Sariq')
+// } else {
+//   console.log('Sariqdan boshqa rangning o`zbekchasini bilmayman')
+// }
+
+// span.position = 'absolute'
+
+// span['display'] = 'block'
+
+// span.border = '2px solid red'
+// console.log(span)
+
+// if ('border' in span) {
+//   span['borderRadius'] = '10px'
+// }
+
+// console.log(span)
+
+// const keys = Object.keys(span)
+
+// const values = Object.values(span)
+// console.log(values)
+
+// const obj = {
+//   7: 'Yaxshi',
+//   9: "Eng a'lo",
+//   6: "Yaxshi o'qimagan",
+//   4: "Umuman o'qimagan",
+// }
+
+// obj[4] = "Yaxshi tayyorlansang bo'lmasmidi"
+// obj[4] = "Sen baribir odam bo'maysan"
+// console.log(obj)
