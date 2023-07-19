dc805a5ef2fbb2c77c26694e651a4eb950c91286
Sherbek Mavlonov <mavlonovsherbek43@gmail.com>
2023-07-19 19:56:47 +0500

    queryselector, queryselectorAll,innerHtml,textContent,innerText,createElement, Practica

---
+++ 25-dars/index.js
@@ -0,0 +1,46 @@
+'use strict'
+// const container = document.getElementsByClassName('container')
+// const tagNames = document.getElementsByTagName('span')
+
+// const lastSpan = document.getElementById('last')
+
+// . = class, # = id
+
+// const container = document.querySelector('.container')
+// class
+// const container = document.querySelectorAll('.container')
+
+// tagname
+// const tagName = document.querySelectorAll('span')
+// console.log(tagName)
+
+// const id = document.querySelector('#last')
+
+// const buttons = document.querySelectorAll('[disabled]')
+
+// const container = document.querySelector('.container')
+// const users = ['John', 'Doe', 'Uik', 'Nimadir', 'Nimadir2']
+
+// const text = element.innerText
+// const text = element.textContent
+// const text = element.innerHTML
+
+// console.log(text)
+
+// element.textContent = '<button>Nimadir</button>'
+
+// element.innerHTML = `<button>
+// Nimadir
+// </button>`
+
+// element.innerText = `<button>Nimadir</button>`
+
+// users.forEach((user) => {
+//   // container.innerHTML += `<div>
+//   // <li>${user}</li>
+//   // <script>${alert('Hello world')}</script>
+//   // </div>`
+//   // const newElement = document.createElement('li')
+//   // newElement.textContent = user
+//   // container.appendChild(newElement)
+// })
